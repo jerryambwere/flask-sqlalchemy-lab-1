3264e206a2642d617cc1ae796a8a09d022b4e02e
Melvin jerry <+>
2024-11-16 08:47:15 +0300

    done

--- server/app.py
+++ server/app.py
@@ -21,6 +21,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake=Earthquake.query.filter(Earthquake.id== id).first()
+    if earthquake:
+        body=earthquake.to_dict()
+        status=200
+    else:
+        body= {'message':f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body,status)
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_magnitude_match(magnitude):
+    quakes=Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    body={
+        'count':len(quakes),
+        'quakes':[quake.to_dict() for quake in quakes ]
+    }
+
+    return make_response(body,200)
+
 
 
 if __name__ == '__main__':
